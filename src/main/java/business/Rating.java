2f31de7dd38df8fd7622885a16ea85e0357c873b
AlexC <alexclinton133@gmail.com>
2024-10-24 17:03:07 +0100

    Added functionality to the Rating class

--- src/main/java/business/Rating.java
+++ src/main/java/business/Rating.java
@@ -1,4 +1,35 @@
 package business;
 
+import lombok.Data;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+/**
+ * Represents a rating given by a user to a song.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Rating {
+    /**
+     *  unique identifier for the rating.
+     */
+    private int ratingId;
+
+    /**
+     *  ID of the user who gave the rating.
+     */
+    private int userId;
+
+    /**
+     *  song ID of the song being rated.
+     */
+    private int songId;
+
+    /**
+     *  rating value (1 to 5).
+     */
+    private int ratingValue;
 }

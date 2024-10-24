3e1efc90b614742484199eadd0b9276063c9a6ab
AlexC <alexclinton133@gmail.com>
2024-10-24 18:45:15 +0100

    Added template for methods in PlaylistDaoImpl and Add PlaylistDao interface defining data access methods for playlist operations

--- src/main/java/persistence/PlaylistDaoImpl.java
+++ src/main/java/persistence/PlaylistDaoImpl.java
@@ -1,6 +1,9 @@
 package persistence;
 
+import business.Playlist;
+
 import java.sql.Connection;
+import java.util.List;
 
 public class PlaylistDaoImpl extends MySQLDao implements PlaylistDao {
     public PlaylistDaoImpl(Connection conn) {
@@ -10,4 +13,47 @@ public class PlaylistDaoImpl extends MySQLDao implements PlaylistDao {
     public PlaylistDaoImpl(String propertiesFilename) {
         super(propertiesFilename);
     }
+
+    @Override
+    public boolean createPlaylist(Playlist playlist) {
+        return false;
+    }
+
+    @Override
+    public boolean updatePlaylist(Playlist playlist) {
+        return false;
+    }
+
+    @Override
+    public boolean addSongToPlaylist(int playlistId, int songId) {
+        return false;
+    }
+
+    @Override
+    public boolean removeSongFromPlaylist(int playlistId, int songId) {
+        return false;
+    }
+
+    @Override
+    public boolean renamePlaylist(int playlistId, String newName) {
+        return false;
+    }
+
+    @Override
+    public Playlist getPlaylistById(int playlistId) {
+        return null;
+    }
+
+//    @Override
+//    public List<Playlist> getUserPlaylists(int userId) {
+//    }
+//
+//    @Override
+//    public List<Playlist> getPublicPlaylists() {
+//    }
+//
+//    @Override
+//    public List<Song> getSongsInPlaylist(int playlistId) {
+//    }
+
 }

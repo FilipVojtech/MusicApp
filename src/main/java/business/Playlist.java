203c95437697844c38956a4418209274bad1cd36
AlexC <alexclinton133@gmail.com>
2024-10-24 17:00:36 +0100

    Added functionality to the playlist class

--- src/main/java/business/Playlist.java
+++ src/main/java/business/Playlist.java
@@ -1,4 +1,42 @@
 package business;
 
+import lombok.Data;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Represents a playlist created by a user.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Playlist {
+    /**
+     *  unique identifier for the playlist.
+     */
+    private int playlistId;
+
+    /**
+     *  name of the playlist.
+     */
+    private String name;
+
+    /**
+     * Indicates if playlist is public or private.
+     */
+    private boolean isPublic;
+
+    /**
+     * The user ID of the playlist owner.
+     */
+    private int userId;
+
+    /**
+     * The list of songs included in the playlist.
+     */
+    private List<Song> songs;
 }

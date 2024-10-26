ec6091c7c9914bb0791d7a42697577b968f19911
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-10-26 18:24:30 +0100

    Add createUser method

--- src/main/java/persistence/UserDaoImpl.java
+++ src/main/java/persistence/UserDaoImpl.java
@@ -24,6 +24,23 @@ public class UserDaoImpl extends MySQLDao implements UserDao {
     }
 
     @Override
+    public boolean createUser(@NonNull User user) {
+        final var sql = "INSERT INTO app_user (email, password, display_name) VALUE (?, ?, ?)";
+
+        try (Connection con = super.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(0, user.getEmail());
+            ps.setString(1, user.getPasswordHash());
+            ps.setString(2, user.getDisplayName());
+
+            return ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            System.out.println("Couldn't create user.");
+        }
+        return false;
+    }
+
+    @Override
     public boolean updateUser(@NonNull User newUserData) {
         if (newUserData.getId() == 0) {
             System.out.println("Cannot update user. ID is not set.");

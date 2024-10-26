50bc3b65e289e419e6b93c816a27b0253bdcb3b8
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-10-26 18:23:17 +0100

    Create Update user method

--- src/main/java/persistence/UserDaoImpl.java
+++ src/main/java/persistence/UserDaoImpl.java
@@ -1,13 +1,42 @@
 package persistence;
 
-import java.sql.Connection;
+import business.User;
+import lombok.NonNull;
 
-public class UserDaoImpl extends MySQLDao implements UserDao{
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * @author Filip Vojtěch
+ */
+public class UserDaoImpl extends MySQLDao implements UserDao {
     public UserDaoImpl(Connection conn) {
         super(conn);
     }
 
     public UserDaoImpl(String propertiesFilename) {
         super(propertiesFilename);
+    }
+
+    @Override
+    public boolean updateUser(@NonNull User newUserData) {
+        if (newUserData.getId() == 0) {
+            System.out.println("Cannot update user. ID is not set.");
+        }
+        final var sql = "UPDATE app_user SET display_name = ?, email = ? WHERE id = ?;";
+
+        try (Connection con = super.getConnection();
+             PreparedStatement statement = con.prepareStatement(sql)) {
+            statement.setString(0, newUserData.getDisplayName());
+            statement.setString(1, newUserData.getEmail());
+            statement.setInt(2, newUserData.getId());
+
+            return statement.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            System.out.println("Couldn't update user");
+        }
+        return false;
     }
 }

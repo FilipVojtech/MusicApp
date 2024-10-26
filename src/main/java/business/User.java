aed0623e171b139658d35baa9e79a37c2848ccfc
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-10-26 18:18:36 +0100

    Implement User class

--- src/main/java/business/User.java
+++ src/main/java/business/User.java
@@ -1,4 +1,19 @@
 package business;
 
+import lombok.*;
+
+/**
+ * @author Filip Vojtěch
+ */
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
+@Builder
 public class User {
+    private int id;
+    private @NonNull String email;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private final @NonNull String passwordHash;
+    private String displayName;
 }

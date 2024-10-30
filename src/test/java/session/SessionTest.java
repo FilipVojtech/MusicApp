a54c1f49f0c5924c4b305b0619650b39e0e151e6
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-10-30 22:53:31 +0000

    IsLoggedIn Session tests

--- src/test/java/session/SessionTest.java
+++ src/test/java/session/SessionTest.java
@@ -1,5 +1,30 @@
 package session;
 
-class SessionTest {
+import business.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionTest {
+    private final static User testUser = new User(1, "test@test.com", "Password", "Test User");
+
+    @BeforeEach
+    void setUp() {
+        Session.clearSession();
+    }
+
+    @Test
+    void isLoggedIn_NoUser() {
+        boolean result = Session.IsLoggedIn();
+        assertFalse(result, "isLoggedIn returned true, expected was false");
+    }
+
+    @Test
+    void isLoggedIn_WithUser() {
+        Session.setUser(testUser);
+
+        boolean result = Session.IsLoggedIn();
+        assertTrue(result, "isLoggedIn returned false, expected was true");
+    }
 }

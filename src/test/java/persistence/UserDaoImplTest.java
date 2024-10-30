b235e1f38bdc94db7ed1f71bc1bbda7657b53d98
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-10-30 22:25:18 +0000

    Implement getUser_ExistingId test method

--- src/test/java/persistence/UserDaoImplTest.java
+++ src/test/java/persistence/UserDaoImplTest.java
@@ -1,5 +1,6 @@
 package persistence;
 
+import business.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,8 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Filip Vojtěch
@@ -35,10 +35,18 @@ class UserDaoImplTest {
 
     @Test
     void getUser_ExistingId() {
-        try (UserDao userDao = new UserDaoImpl(conn)) {
-        } catch (Exception e) {
-            fail("Could not init UserDao");
-        }
+        int userId = 1;
+        UserDao userDao = new UserDaoImpl(conn);
+        User expectedUser = new User(
+                1,
+                "user1@example.com",
+                "$2a$14$Sci/QKCO4xEAqh2O3/PSk.XGWdEF.Jxy/AB0cDCkGWu19Gz1d3gPq",
+                "User One"
+        );
+        User user = userDao.getUser(userId);
+
+        assertNotNull(user);
+        assertEquals(expectedUser, user);
     }
 
     @Test

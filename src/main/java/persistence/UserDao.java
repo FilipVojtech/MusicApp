7953181b950f75bc839b2af5069fa1f9c80211d2
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-10-30 18:20:13 +0000

    Update comments to include throws clause

--- src/main/java/persistence/UserDao.java
+++ src/main/java/persistence/UserDao.java
@@ -11,22 +11,28 @@ import persistence.exceptions.RecordNotFound;
 public interface UserDao extends AutoCloseable {
     /**
      * Retrieves a user by their ID
+     *
      * @param id ID to look up by
      * @return A new {@systemProperty User} object. Null if the user couldn't be found
+     * @throws RecordNotFound When the user is not found
      */
     User getUser(@NonNull int id) throws RecordNotFound;
 
     /**
      * Looks up user by their email
+     *
      * @param email Email to look up by
      * @return The found user. Null if none found
+     * @throws RecordNotFound When the user is not found
      */
     User getUserByEmail(@NonNull String email) throws RecordNotFound;
 
     /**
      * Creates a new user.
+     *
      * @param user The new user data
      * @return True if create succeeded. False otherwise.
+     * @throws EmailAddressAlreadyUsed When the created user has an email address already used by a different user.
      */
     boolean createUser(@NonNull User user) throws EmailAddressAlreadyUsed;
 
